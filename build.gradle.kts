3726001e349878b298bc01013ebc50a8f70ca53a
Steve Storck <steve973@gmail.com>
2024-05-18 11:26:42 -0400

    Moved sonar config to main build file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,17 @@
+plugins {
+    id("org.sonarqube")
+}
 group = "org.storck"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Steve973_simplex-consensus-protocol")
+        property("sonar.organization", "steve973")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
